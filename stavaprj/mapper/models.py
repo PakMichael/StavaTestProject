34b358a3b8396157475cdb203bc26876c1117ce0
Misha Pak <tenoremike@gmail.com>
2021-02-28 15:15:39 +0100

    Declaring model

--- stavaprj/mapper/models.py
+++ stavaprj/mapper/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Mapper(models.Model):
+    request_id = models.CharField(max_length=128, unique=True)
+    start_req_time = models.DateTimeField(null=True)
+    end_req_time = models.DateTimeField(null=True)
